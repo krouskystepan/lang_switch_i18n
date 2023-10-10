09d2d7cba31c9aedceeb2b3643b6151dad94ab29
Stepan Krousky <stepan.krousky@seznam.cz>
2023-10-10 22:25:55 +0200

    cz

---
+++ langs/cs.ts
@@ -0,0 +1,16 @@
+export const cs = {
+    navigation: {
+      home: 'Domů',
+      about: 'O nás'
+    },
+    page: {
+      home: {
+        title: 'Vítej v NextJs i18n',
+        description: 'Internacionalizace v routeru aplikací NextJs 13'
+      },
+      about: {
+        title: 'O nás',
+        description: 'Toto je stránka o nás'
+      }
+    }
+  }
--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -2,6 +2,7 @@ import 'server-only'
 import type { Locale } from '@/i18n.config'
 
 const dictionaries = {
+  cs: async () => ((await import('../langs/cs')).cs),
   en: async () => ((await import('../langs/en')).en),
   de: async () => ((await import('../langs/de')).de)
 }
