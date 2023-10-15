f38b99e519d8a7acc385df04774103130a7d2177
Stepan Krousky <stepan.krousky@seznam.cz>
2023-10-15 19:53:38 +0200

    add dynamic values, pluralization

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -10,6 +10,14 @@ export default function Index() {
   return (
     <>
       <h1>{t('page.title')}</h1>
+      <h4>{t('page.dynamic', { name: 'Jane' })}</h4>
+      <h4>{t('page.plural', { count: 0 })}</h4>
+      <h4>{t('page.plural', { count: 1 })}</h4>
+      <h4>{t('page.plural', { count: 2 })}</h4>
+      <h4>{t('page.plural', { count: 3 })}</h4>
+      <h4>{t('page.plural', { count: 4 })}</h4>
+      <h4>{t('page.plural', { count: 5 })}</h4>
+      <h4>{t('page.plural', { count: 20 })}</h4>
       {locales.map((locale) => (
         <Link locale={locale} href="/">
           <p>{locale}</p>
