54a7a59f6ace510392686e295b36b3acebd54079
Stepan Krousky <stepan.krousky@seznam.cz>
2023-10-15 19:29:23 +0200

    lang switch

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,8 @@
 import { ClientSide } from '@/components/ClientSide';
 import { ServerSide } from '@/components/ServerSide';
+import { locales } from '@/middleware';
 import { useTranslations } from 'next-intl';
+import Link from 'next-intl/link';
 
 export default function Index() {
   const t = useTranslations();
@@ -8,6 +10,11 @@ export default function Index() {
   return (
     <>
       <h1>{t('page.title')}</h1>
+      {locales.map((locale) => (
+        <Link locale={locale} href="/">
+          <p>{locale}</p>
+        </Link>
+      ))}
       <ClientSide
         message={{
           title: t('clientSide.title'),
